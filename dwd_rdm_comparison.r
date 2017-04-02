04ca983e921b0f96adc9c1e8774de083a3748e32
Sebastian Schmidt <isibboi@gmail.com>
2017-04-02 16:13:05 +0300

    Created script to compare data between dwd and rdm

--- dwd_rdm_comparison.r
+++ dwd_rdm_comparison.r
@@ -5,11 +5,7 @@ library('RSQLite')
 connection <- dbConnect(drv='SQLite', dbname='../weather.sqlite')
 alltables <- dbListTables(connection)
 
-alltables
-
 dwd_rdm_data <- dbGetQuery(connection, 'SELECT dwdweatherdata.timestamp AS timestamp, dwdweatherdata.temperature AS dwdtmp, dwdweatherdata.humidity AS dwdhum, dwdweatherdata.pressure as dwdpres, rdmweatherdata.temperature AS rdmtmp, rdmweatherdata.humidity AS rdmhum, rdmweatherdata.pressure as rdmpres FROM dwdweatherdata INNER JOIN rdmweatherdata ON dwdweatherdata.timestamp = rdmweatherdata.timestamp')
-
-dwd_rdm_data
 
 x11()
 plot(dwd_rdm_data$dwdtmp, dwd_rdm_data$rdmtmp, xlab='Temperature measured by DWD', ylab='Temperature measured by RDM private weather station')
